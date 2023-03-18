dc4d77d2c6d385cb8cbc3e39f04c44a7771fca58
asyereal <asyereal@protonmail.com>
2023-03-18 11:14:59 +0800

    added new menulevel

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const path = require("path");
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, Menu} = require("electron");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const isWin = process.platform === "win32";
+const isGNU = process.platform === "linux";
+const isMac = process.platform === "darwin";
+
 
 function createMainWindow(){
 	const mainWindow = new BrowserWindow({
@@ -8,9 +15,62 @@ function createMainWindow(){
 		height: 600
 	});
 
+	/*
+	if(isDev){
+		mainWindow.webContents.openDevTools();
+	}
+	*/
+
 	mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+function createAboutWindow(){
+	const aboutWindow = new BrowserWindow({
+		title: "About RealEdu",
+		width: 300,
+		height: 300
+	});
+
+	aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+
 }
 
 app.whenReady().then(() => {
 	createMainWindow();
+
+	app.on("activate", () => {
+		if(BrowserWindow.getAllWindows().length === 0) {
+			createMainWindow();
+		}
+	})
+	const mainMenu = Menu.buildFromTemplate(menu);
+	Menu.setApplicationMenu(mainMenu);
 });
+
+app.on("window-all-closed", () => {
+	if(!isMac){
+		app.quit()
+	}
+})
+
+const menu = [
+	...(isMac ? [{
+		label: app.name,
+		submenu: [
+			{
+				label: "About",
+				click: createAboutWindow
+			}
+		]
+	}] : []),
+	{
+		role: "fileMenu",
+	},
+	...(!isMac ? [{
+		label: "Help",
+		submenu: [{
+			label: "About",
+			click: createAboutWindow
+		}]
+	}] : [])
+];
