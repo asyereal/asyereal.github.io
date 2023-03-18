fac7720f208027f4bb332f85fd506b8412154005
asyereal <asyereal@protonmail.com>
2023-03-18 14:10:39 +0800

    added template

--- renderer/script.js
+++ renderer/script.js
@@ -1,18 +1,11 @@
 var language = "en";
 var edulevel = "primary"
-function toPs(){
-	document.getElementById("descbar").innerHTML = "Sekolah rendah - placeholder kosong1 -";
-	document.getElementById("langprompt").style.display = "none";
-}
-function toSs(){
-	document.getElementById("descbar").innerHTML = "Sekolah menengah - placeholder kosong 2 -";
-	document.getElementById("langprompt").style.display = "none";
-}
 function opentab(a){
 	var i;
 	var x = document.getElementsByClassName("maintabs");
 	for(i = 0; i < x.length; i++){
 		x[i].style.display = "none";
 	}
+	document.getElementById("eduprompt").style.display = "none";
 	document.getElementById(a).style.display = "block";
 }
